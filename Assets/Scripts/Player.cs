10f7bda8d054c8fa4ad6affd72b6f96529a67f73
Luis Rodriguez <developer0606@gmail.com>
2021-03-04 20:02:35 -0400

    Add Aim

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,6 +9,9 @@ public class Player : MonoBehaviour
 
     Vector3 moveDirection;
     [SerializeField] float speed = 3;
+    [SerializeField] Transform aim;
+    [SerializeField] Camera camera;
+    Vector2 facingDirection;
 
     // Start is called before the first frame update
     void Start()
@@ -19,10 +22,17 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         h = Input.GetAxis("Horizontal");
         v = Input.GetAxis("Vertical");
         moveDirection.x = h;
         moveDirection.y = v;
+        
         transform.position += moveDirection * Time.deltaTime * speed;
+
+        //Movimiento de la mira
+        facingDirection = camera.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        aim.position = transform.position + (Vector3)facingDirection.normalized;
+
     }
 }
